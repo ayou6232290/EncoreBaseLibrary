d177fa02740617204fadf85975fe4b8a76a7ba4c
Encore <6232290@qq.com>
2014-01-07 17:18:07 +0800

    增加拼接方法

--- src/cn/encore/lib/utils/Util.java
+++ src/cn/encore/lib/utils/Util.java
@@ -9,4 +9,30 @@ public class Util {
 		return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, res.getDisplayMetrics());
 	}
 
+	/**
+	 * 拼接从图片资源文件拼接URI
+	 * @param resId
+	 * @return
+	 */
+	public static String getImageCacheByResIdUri(int resId){
+		return "drawable://" + resId;
+	}
+	
+	/**
+	 * 拼接从asset文件拼接URI
+	 * @param resId
+	 * @return
+	 */
+	public static String getImageCacheByAssetIdUri(String assetFileName){
+		return "assets://" + assetFileName;
+	}
+	
+	/**
+	 * 拼接从内存卡拼接URI
+	 * @param resId
+	 * @return
+	 */
+	public static String getImageCacheBySdcardIdUri(String fileName){
+		return "file://" + fileName;
+	}
 }
